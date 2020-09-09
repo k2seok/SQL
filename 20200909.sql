7f66c4568736dd719900d3ada8e11291db8e8735
Yoon Ki Seok <yks5313@gmail.com>
2020-09-09 23:50:23 +0900

    Update 20200909.sql

--- 20200909.sql
+++ 20200909.sql
@@ -1,8 +1,8 @@
  SELECT  E.data1 | ‘,’ | E.data2. | ‘,’ | E.data3 FROM (
 SELECT
-  data1
-  ,data2
+  A.data1
+  ,A.data2
   ,(SELECT B.data3 FROM B002 B WHERE B.data1 = A.data1 ) data3
- FROM A001 A WHERE data1 = 'aa' AND data2 > '2'
+ FROM A001 A WHERE A.data1 = 'aa' AND A.data2 > '2'
 ) E
   
